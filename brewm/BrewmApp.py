961ce9aa6c3ec358aaa127e11727162402004dcf
Steve Baker <steve@stevebaker.org>
2009-04-25 20:57:39 +1200

    hello world

--- brewm/BrewmApp.py
+++ brewm/BrewmApp.py
@@ -8,8 +8,21 @@
 #
 #-------------------------------------------------------------------------------
 
-import pygtk
-pygtk.require("2.0")
-import gtk.glade, gobject
 import sys
-import os, os.path
+import gtk
+  
+class BrewmApp:
+    def on_window_destroy(self, widget, data=None):
+        gtk.main_quit()
+    
+    def __init__(self):
+        builder = gtk.Builder()
+        builder.add_from_file("Brewm.glade") 
+        
+        self.window = builder.get_object("window")
+        builder.connect_signals(self)
+    
+if __name__ == "__main__":
+    app = BrewmApp()
+    app.window.show()
+    gtk.main()
